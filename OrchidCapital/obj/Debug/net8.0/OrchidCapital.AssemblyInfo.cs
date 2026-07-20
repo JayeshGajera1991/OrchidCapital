7740f2415d7f390e9bc86bf1cf8fcddb538f67fb
JayeshGajera1991 <gajerajayesh1991@gmail.com>
2026-07-20 13:18:01 +0530

    update all btn

--- OrchidCapital/obj/Debug/net8.0/OrchidCapital.AssemblyInfo.cs
+++ OrchidCapital/obj/Debug/net8.0/OrchidCapital.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrchidCapital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a3a957d292c76c049a898d7280baccbfefac6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7a69b291cd2970da5d0456f9a123843ac7edc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrchidCapital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrchidCapital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
